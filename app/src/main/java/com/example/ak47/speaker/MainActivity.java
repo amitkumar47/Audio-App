5ead273adfdc8eafb61dd93169ef1e3dc57df3a1
amitkumar47 <amits4787@gmail.com>
2019-02-14 15:53:32 +0530

    used volley library to see response of url

--- app/src/main/java/com/example/ak47/speaker/MainActivity.java
+++ app/src/main/java/com/example/ak47/speaker/MainActivity.java
@@ -4,23 +4,52 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer song;
+    RequestQueue queue;
+    private TextView res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        song = MediaPlayer.create(MainActivity.this, R.raw.si);
-        song.start();
+
     }
     public void submit(View view){
-        song.release();
+
+        res =(TextView)findViewById(R.id.res) ;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://www.google.com/";
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        res.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                  res.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
     @Override
     protected void onPause(){
         super.onPause();
-        song.release();
+
     }
 }
